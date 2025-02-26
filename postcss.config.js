bc4914f1f11eb131a44c39b73e71ea92e15d494c
P. Edmon Graham <paul@edmon.zip>
2025-02-25 23:39:42 -0600

    homepage and some minor tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,8 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [
-      require('cssnano')({
+    require('cssnano')({  
         preset: 'default'
-      })
-    ] : [])
+    })
   ]
 } 
